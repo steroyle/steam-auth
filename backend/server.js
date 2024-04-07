f0f9a86f1b95ab6f85c3c7bb76501723438d4f50
steroyle <steroyle@gmail.com>
2024-04-07 21:15:29 +0100

    Added passport package and and routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,41 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import passport from 'passport';
+import { Strategy as SteamStrategy } from 'passport-steam';
 
 dotenv.config();
-
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Set up your Steam strategy here
+// You need to replace 'YOUR_STEAM_API_KEY' and 'YOUR_RETURN_URL'
+passport.use(new SteamStrategy({
+  returnURL: 'http://localhost:5000/auth/steam/return',
+  realm: process.env.APP_DOMAIN,
+  apiKey: process.env.STEAM_API_KEY
+},
+function(identifier, profile, done) {
+  User.findOrCreate({ steamId: profile.id }, function (err, user) {
+    return done(err, user);
+  });
+}
+));
+
+// Route to start the authentication process
+app.get('/auth/steam', passport.authenticate('steam', { failureRedirect: '/' }), function(req, res) {
+  res.redirect('/');
+});
+
+// Route for Steam to redirect back to after logging in
+app.get('/auth/steam/return',
+  passport.authenticate('steam', { failureRedirect: '/' }),
+  function (req, res) {
+    console.log(`Returned from Steam`);
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  }
+);
+
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello from the backend!'});
